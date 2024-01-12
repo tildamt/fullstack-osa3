97cfca39ba405cd82d35abab3b9254fc1d68fc3a
tildamt <128397429+tildamt@users.noreply.github.com>
2024-01-12 14:25:46 +0200

    Exercise 3.1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      }
+    ]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
